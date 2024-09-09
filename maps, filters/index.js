f96f1b816b3ef16fe54c06bdf800e29099961947
siddhi_1305 <siddhisharma4549@gmail.com>
2024-09-09 10:38:46 +0530

    maps nd filters

---
+++ maps, filters/index.js
@@ -0,0 +1,25 @@
+//maps, filters, arrow function
+//given an array, give me back a new array in hich every value is multiplied by 2'
+//[1, 2, 3, 4, 5]
+//[2, 4, 6, 8, 10]
+
+const input = [1, 2, 3, 4, 5];
+
+// const newArray = [];
+
+// for (let i=0 ; i<input.length; i++){
+//     newArray.push(input[i]*2);
+// }
+// console.log(newArray);
+
+//other solution
+
+function transform(i){
+    return i*2;
+}
+
+const ans= input.map(transform);
+console.log(ans);
+
+
+//filtering
